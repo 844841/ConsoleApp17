50aa40f8e84d3e69290eda729e9ba842a5deb921
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 14:56:59 +0530

    Initial commit

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sampleclasslibrary;
+
+namespace sampleclasslibrary
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            recarea a = new recarea();
+            a. length = 404;
+            a.breadth = 50;
+            Console.WriteLine(a.Area());
+
+        }
+    }
+}
